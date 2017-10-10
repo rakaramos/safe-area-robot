131787c526bc9f2e8906f5eb208a3fa096d4d389
Rafael Ramos <raka.cdc@gmail.com>
2017-10-10 11:08:08 -0300

    Print current file path

--- safeArea.sh
+++ safeArea.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
+
 if [ -n "$1" ]; then
 	current="$(pwd)"
 
     cd "$1"
 
     find "$(cd .; pwd)" \( -name "*.storyboard" -o -name "*.xib" \) -print0 | xargs -0 egrep -r -o -L -e "useSafeAreas=\"YES\"" --include "*.storyboard" --include "*.xib" | while read line; do
+    	echo "Processing $line";
         osascript "$current""/safeArea.applescript" --args "$line"
 	done
 
